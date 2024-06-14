4d254a29f759d4da218f72525f8d3676c6393871
Wills Manley <wmanley@pinterest.com>
2024-06-14 15:04:26 -0500

    misc

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-import { useEffect, useRef, useState } from 'react';
-import { Box, Flex, Image, Masonry } from 'gestalt';
+import { useCallback, useRef, useEffect, useState } from "react";
+import { Box, Flex, Image, Masonry } from "gestalt";
+
+const randomPinHeight = () => Math.random() * 200 + 100;
 
 // Generate skeleton pins
-const skeletonPins = [...new Array(3)].map(() => [
-  { id: 1, height: 316, width: 474 },
-  { id: 2, height: 1081, width: 474 },
-  { id: 3, height: 711, width: 474 },
-  { id: 4, height: 632, width: 474 },
-  { id: 5, height: 710, width: 474 },
-]).flat();
+const skeletonPins = [...new Array(3)]
+  .map(() => [
+    { id: 1, height: randomPinHeight(), width: 474 },
+    { id: 2, height: randomPinHeight(), width: 474 },
+    { id: 3, height: randomPinHeight(), width: 474 },
+    { id: 4, height: randomPinHeight(), width: 474 },
+    { id: 5, height: randomPinHeight(), width: 474 },
+  ])
+  .flat()
 
 // Function to get pins
 function getPins() {
   const pins = [
     {
-      color: '#2b3938',
+      color: "#2b3938",
       height: 316,
-      src: 'https://i.ibb.co/sQzHcFY/stock9.jpg',
+      src: "https://i.ibb.co/sQzHcFY/stock9.jpg",
       width: 474,
-      name: 'the Hang Son Doong cave in Vietnam',
+      name: "the Hang Son Doong cave in Vietnam",
     },
     {
-      color: '#8e7439',
+      color: "#8e7439",
       height: 1081,
-      src: 'https://i.ibb.co/zNDxPtn/stock10.jpg',
+      src: "https://i.ibb.co/zNDxPtn/stock10.jpg",
       width: 474,
-      name: 'La Gran Muralla, Pekín, China',
+      name: "La Gran Muralla, Pekín, China",
     },
     {
-      color: '#698157',
+      color: "#698157",
       height: 711,
-      src: 'https://i.ibb.co/M5TdMNq/stock11.jpg',
+      src: "https://i.ibb.co/M5TdMNq/stock11.jpg",
       width: 474,
-      name: 'Plitvice Lakes National Park, Croatia',
+      name: "Plitvice Lakes National Park, Croatia",
     },
     {
-      color: '#4e5d50',
+      color: "#4e5d50",
       height: 632,
-      src: 'https://i.ibb.co/r0NZKrk/stock12.jpg',
+      src: "https://i.ibb.co/r0NZKrk/stock12.jpg",
       width: 474,
-      name: 'Ban Gioc – Detian Falls : 2 waterfalls straddling the Vietnamese and Chinese border.',
+      name: "Ban Gioc – Detian Falls : 2 waterfalls straddling the Vietnamese and Chinese border.",
     },
     {
-      color: '#6d6368',
+      color: "#6d6368",
       height: 710,
-      src: 'https://i.ibb.co/zmFd0Dv/stock13.jpg',
+      src: "https://i.ibb.co/zmFd0Dv/stock13.jpg",
       width: 474,
-      name: 'Border of China and Vietnam',
+      name: "Border of China and Vietnam",
     },
   ];
 
@@ -70,56 +74,47 @@ function GridComponent({ data }) {
 }
 
 // Component to display a skeleton pin
-function SkeletonPin({ height, width }) {
-  const skeletonRef = useRef();
-  const [delay, setDelay] = useState(0);
-
-  useEffect(() => {
-    const calculateDelay = () => {
-      if (skeletonRef.current) {
-        const rect = skeletonRef.current.getBoundingClientRect();
-        const delayInSeconds = (rect.left + rect.top) / 100;
-        setDelay(delayInSeconds);
+function SkeletonPin({ height }) {
+  const refCallback = useCallback((ref) => {
+    // rewrite parent to use abs vs transform
+    if (ref) {
+      const parentEl = ref.parentElement;
+      if (parentEl.style.transform) {
+        const styles = getComputedStyle(parentEl);
+        if (styles.transform) {
+          const matrix = new DOMMatrix(styles.transform);
+          const { m41: x, m42: y } = matrix;
+          parentEl.style.transform = "";
+          parentEl.style.top = `${y}px`;
+          parentEl.style.left = `${x}px`;
+        }
       }
-    };
-
-    calculateDelay();
-    window.addEventListener('resize', calculateDelay);
-
-    return () => {
-      window.removeEventListener('resize', calculateDelay);
-    };
+    }
   }, []);
-
   return (
-    <div
-      ref={skeletonRef}
-      style={{
-        animationDelay: `${delay}s`,
-        animationDuration: `${width / 100}s`,
-      }}
-      id="skeleton"
-      className="skeleton"
-    >
-      <Box width={width} height={height} color="lightGray" />
+    <div className="skeleton" ref={refCallback}>
+      <Box width="100%" height={height} color="lightGray" />
     </div>
   );
 }
 
 // Main component
-export default function Example() {
+export default function App() {
   const [pins, setPins] = useState(skeletonPins);
   const scrollContainerRef = useRef();
   const gridRef = useRef();
 
   useEffect(() => {
     setTimeout(() => {
-      // getPins().then(setPins);
-    }, 1000);
+      getPins().then(setPins);
+    }, 2500);
   }, []);
 
   return (
-    <div ref={scrollContainerRef} style={{ overflowY: 'scroll', height: '100vh' }}>
+    <div
+      ref={scrollContainerRef}
+      style={{ overflowY: "scroll", height: "100vh" }}
+    >
       <Masonry
         ref={(ref) => {
           gridRef.current = ref;
